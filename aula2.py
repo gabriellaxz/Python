5b8cc6ea6f2295644d9ceb8d794024d9ff0867f0
gabriellaxz <79538335+gabriellaxz@users.noreply.github.com>
2021-05-07 23:18:51 -0300

    Add files via upload

---
+++ aula2.py
@@ -0,0 +1,31 @@
+
+a = float(input('Entre com o primeiro valor: '))
+b = float(input('Entre com o segundo valor: '))
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+resto = a % b
+print('Soma: {soma}. \nSubtracao: {subtracao}.'
+      '\nMultiplicacao: {multiplicacao}'
+       '\nDivisao: {divisao}'
+      '\nResto: {resto}' .format(soma=soma,
+                                  subtracao=subtracao ,
+                                  resto=resto ,
+                                  multiplicacao=multiplicacao ,
+                                  divisao=divisao))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
